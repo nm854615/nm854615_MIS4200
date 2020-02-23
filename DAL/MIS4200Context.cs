ca71307a899e4ae6c4e26a427dbe481ad8b50149
Nathan Morabeto <nm854615@ohio.edu>
2020-02-23 12:51:14 -0500

    2/23

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -9,7 +9,7 @@ namespace nm854615_MIS4200.DAL
 {
     public class MIS4200Context : DbContext // inherits from DbContext
     {
-        public MIS4200Context() : base("name=DefaultConnection")
+        public MIS4200Context() : base("name=MIS4200")
         {
             // this method is a 'constructor' and is called when a new context is created
             // the base attribute says which connection string to use
